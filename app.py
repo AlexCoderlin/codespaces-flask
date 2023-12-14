e932a93aa8e4049e2ba1b3bcb1b15d99bd13f0ab
Alejandro Gonzalez Colin <ekmmoproductions@gmail.com>
2023-12-14 00:07:07 +0000

    mensaje

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
-# app.py
-from flask import Flask, render_template, request, Response
+from flask import Flask, request, render_template, Response
 import requests
-from io import BytesIO
-from gzip import GzipFile
+from bs4 import BeautifulSoup
+import zlib
 
 app = Flask(__name__)
+
+def compress(content):
+    return zlib.compress(content.encode('utf-8'), level=zlib.Z_BEST_COMPRESSION)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/youtube-proxy', methods=['POST'])
-def youtube_proxy():
-    youtube_url = request.form.get('url')
-    
-    # Descarga el video de YouTube
-    response = requests.get(youtube_url, stream=True)
-    
-    # Comprime el contenido
-    compressed_content = compress_content(response.content)
-    
-    # Configura la respuesta con el contenido comprimido
-    compressed_response = Response(compressed_content)
-    compressed_response.headers['Content-Type'] = 'video/mp4'
-    compressed_response.headers['Content-Encoding'] = 'gzip'
-    
-    return compressed_response
+@app.route('/proxy', methods=['POST'])
+def proxy():
+    url = request.form['url']
 
-def compress_content(content):
-    buffer = BytesIO()
-    with GzipFile(fileobj=buffer, mode='wb') as f:
-        f.write(content)
-    return buffer.getvalue()
+    # Realiza la solicitud a la URL proporcionada
+    response = requests.get(url)
+
+    # Comprueba si la respuesta ya está comprimida
+    if 'Content-Encoding' in response.headers and 'gzip' in response.headers['Content-Encoding']:
+        return Response(response.content, content_type=response.headers['Content-Type'])
+
+    # Comprime el contenido de la página
+    compressed_content = compress(response.text)
+
+    # Renderiza la página utilizando BeautifulSoup para optimizarla
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Puedes aplicar diversas optimizaciones aquí según tus necesidades
+
+    optimized_content = str(soup)
+
+    # Devuelve la página comprimida y optimizada al cliente
+    return Response(compressed_content, content_type='text/html; charset=utf-8', headers={'Content-Encoding': 'gzip'})
 
 if __name__ == '__main__':
     app.run(debug=True)
